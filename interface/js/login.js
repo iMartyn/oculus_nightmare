5676ee0e6c22cf3d508d5bed2c8dc20e13080801
shi <notshi@gmail.com>
2013-08-11 12:19:02 +0100

    myuserdata is now a global variable.

--- interface/js/login.js
+++ interface/js/login.js
@@ -3,7 +3,9 @@ jQuery(document).ready(function () {
         var this_location = document.location.toString();
         var base_url = this_location.replace(/\/interface.*/,'');
         var name=jQuery("#loginame").val();
-        var action_url = base_url + '/server/?command=' + "login";
-        jQuery.get(action_url+"&name="+name);
+        var action_url = base_url + '/server/?command=' + "client_register";
+        jQuery.getJSON(action_url+"&name="+name,function(data) {
+			myuserdata = data;
+			});
     });
 });
