3d379fa9ab8c5d0dfd08cc901bbc8e6cc85a7c98
Paul Brook <paul@nowt.org>
2013-08-11 08:35:20 +0100

    Move and rotate map.
    Signed-off-by: Paul Brook <paul@nowt.org>

--- interface/js/buttons.js
+++ interface/js/buttons.js
@@ -1,9 +1,13 @@
+function command_url(command) {
+    var this_location = document.location.toString();
+    var base_url = this_location.replace(/\/interface.*/,'');
+    return base_url + '/server/?command=' + command;
+}
+
 jQuery(document).ready(function () {
     jQuery("i").click(function(sender) {
-        var this_location = document.location.toString();
-        var base_url = this_location.replace(/\/interface.*/,'');
-        var command = jQuery(sender.delegateTarget).attr('id');
-        var action_url = base_url + '/server/?command=' + command;
+	var command = jQuery(sender.delegateTarget).attr('id');
+        var action_url = command_url(command);
         jQuery.get(action_url);
     });
 });
--- interface/js/map.js
+++ interface/js/map.js
@@ -3,6 +3,14 @@ var brooklyn = new google.maps.LatLng(53.80134, -1.53687) // really not
 
 var MY_MAPTYPE_ID = 'custom_style';
 
+function update_map(data) {
+    var latlng = new google.maps.LatLng(data.latitude, data.longitude);
+    map.panTo(latlng);
+    if ("bearing" in data) {
+      jQuery("#map-canvas").css("transform","rotate(" + (-data.bearing).toString() + "deg)");
+    }
+}
+
 function initialize() {
 
   var mapOptions = {
@@ -36,6 +44,9 @@ function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
+  setInterval(function() {
+      jQuery.getJSON(command_url("status"), update_map);
+  }, 1000);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
