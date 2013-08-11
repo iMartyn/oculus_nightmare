1956153ace9748fb4d19aa2e83e945ca31dfca28
Paul Brook <paul@nowt.org>
2013-08-11 12:38:23 +0100

    Make voting work - mostly.
    Signed-off-by: Paul Brook <paul@nowt.org>

--- interface/js/vote.js
+++ interface/js/vote.js
@@ -1,6 +1,19 @@
 
+function command_url(command) {
+    var this_location = document.location.toString();
+    var base_url = this_location.replace(/\/interface.*/,'');
+    return base_url + '/server/?command=' + command;
+}
+
+var game_id = 0;
+
 jQuery(document).ready(function () {
     jQuery(".action-butt").click(function (sender) {
-	console.log(jQuery(this).attr("id")[5]);
+	var voteid=jQuery(this).attr("id")[5];
+	var userid = myuserdata.id.toString()
+	jQuery.getJSON(command_url("client_vote") + "&user_id=" + userid + 
+	  "&game_id=" + game_id + "&vote=" + voteid, function(data) {
+	      // Don't care.
+	});
     });
 });
